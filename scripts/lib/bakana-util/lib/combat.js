bbc8eff3f9301a7b5e36dfa49a32b25295f3e523
Bakana Baka <bakanabaka.vtt@gmail.com>
2024-12-20 14:04:54 -0600

    Update combat API (breaks automations)

--- scripts/lib/bakana-util/lib/combat.js
+++ scripts/lib/bakana-util/lib/combat.js
@@ -1,27 +1,90 @@
-function getCombatInfo() {
+/* Example usages of framework
+ *
+ * >>  An item, allowed to be used up to 3 times per round, but only on other token's turns
+ * if (!macroUtil.combat.isAllowed(actor, macroItem, macroItem.name, {frequency = "round", restriction = "other", allowed = 3})) return;
+ * await macroUtil.combat.setInfo(macroItem, macroItem.name);
+ *   < Do item stuff >
+ * 
+ * >> An item, allowed to be used once per combat, but only on your own turn
+ * * if (!macroUtil.combat.isAllowed(actor, macroItem, macroItem.name, {frequency = "combat", restriction = "own", allowed = 1})) return;
+ * await macroUtil.combat.updateInfo(macroItem, macroItem.name);
+ *   < Do item stuff >
+ * 
+ * Note: outsideCombat behaves poorly with per combat, actions taken before combat start to not count against actions in combat.
+ *       It is recommended if you are checking per-combat to set outsideCombat = false
+ */
+
+function _initInfo() {
     return {
-        active: game.combat?.active,
-        round: game.combat?.round,
-        turn: game.combat?.turn,
-        id: game.combat?.id,
+        turn:   {id: game.combat?.turn,  count: 1},
+        round:  {id: game.combat?.round, count: 1},
+        combat: {id: game.combat?.id,    count: 1}
     };
 }
 
-function isSameTurn(combatInfo) {
-    if (!game.combat) return false;
-    if (combatInfo.id != game.combat.id) return false;
-    if (combatInfo.round != game.combat.round) return false;
-    return combatInfo.turn == game.combat.turn;
+function _update(combatInfo) {
+    if (!combatInfo) return _initInfo();
+    let updates = foundry.utils.duplicate(combatInfo);
+
+    updates.turn.count++;
+    updates.round.count++;
+    updates.combat.count++;
+    
+    if (updates?.turn.id != game.combat?.turn) updates.turn.count--;
+    if (updates?.round.id != game.combat?.round) updates.round.count--;
+    if (updates?.combat.id != game.combat?.id) updates.combat.count--;
+    return updates;
 }
 
-function isSameRound(combatInfo) {
-    if (!game.combat) return false;
-    if (combatInfo.id != game.combat.id) return false;
-    return combatInfo.round == game.combat.round;
+function _isSameTurn(combatInfo) {
+    if (!macroUtil.combat.isSameRound(combatInfo)) return false;
+    return combatInfo?.turn.id == game.combat?.turn;
+}
+
+function _isSameRound(combatInfo) {
+    if (!macroUtil.combat.isSameCombat(combatInfo)) return false;
+    return combatInfo?.round.id == game.combat.round;
+}
+
+function _isSameCombat(combatInfo) {
+    return combatInfo?.combat.id == game.combat?.id;
+}
+
+function isAllowed(token, document, id, {frequency = "turn", restriction = "none", allowed = 1, outsideCombat = true} = {}) {
+    // outside combat allowance
+    let combatInfo = document.getFlag('world', 'combat') ?? {};
+    let specificInfo = combatInfo[id];
+
+    if (!outsideCombat) {
+        // if we are not in active combat, warn user and return false
+        if (!game.combat?.active) {
+            ui.notifications.warn('This can not be used outside of combat.');
+            return false;
+        }
+    }
+
+    switch (restriction) {
+        case "none":  break;  // no restriction, continue
+        case "own":   if (game.combat?.current.tokenId == token.id) { break; } else { return false; };
+        case "other": if (game.combat?.current.tokenId != token.id) { break; } else { return false; };
+        default: throw(`Unknown restriction: ${restriction}`);
+    }
+
+    switch (frequency) {
+        case "combat":  return !_isSameCombat(specificInfo) || (specificInfo.combat.count < allowed);
+        case "round":   return !_isSameRound(specificInfo)  || (specificInfo.round.count  < allowed);
+        case "turn":    return !_isSameTurn(specificInfo)   || (specificInfo.turn.count   < allowed);
+        default: throw(`Unknown frequency: ${frequency}`);
+    }
+}
+
+async function updateInfo(document, id) {
+    let info = document.getFlag('world', 'combat') ?? {};
+    info[id] = _update(info[id]);
+    await document.setFlag('world', 'combat', info);
 }
 
 export const combatApi = {
-    getCombatInfo,
-    isSameTurn,
-    isSameRound,
+    updateInfo,
+    isAllowed
 };
