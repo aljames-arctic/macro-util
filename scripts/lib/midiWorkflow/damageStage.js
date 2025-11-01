f64cfde95887d0ddeab9bab571aa2966692aa48f
aljames-arctic <bakanabaka.vtt@gmail.com>
2025-10-31 23:49:14 -0500

    feat: workflow.isSlain
    Signed-off-by: aljames-arctic <bakanabaka.vtt@gmail.com>

--- scripts/lib/midiWorkflow/damageStage.js
+++
@@ -1,6 +0,0 @@
-async function isSlain(workflow) {
-    const damageItem = workflow.damageItem;
-    return (damageItem.newHP == 0) && (damageItem.oldHp !== 0);
-}
-
-export const damageApi = { isSlain };
--- scripts/lib/workflow.js
+++ scripts/lib/workflow.js
@@ -1,30 +1,58 @@
-/**
- * Applies damage to a token from an item.
- * @param {object} itemData The data for the item to apply damage from.
- * @param {Array<Array<string>>} damageParts The parts of the damage to apply.
- * @param {object} token The token to apply damage to.
- * @returns {Promise<void>}
- */
-async function itemDamageToken(itemData, damageParts, token) {
-    let itemUpdate = foundry.utils.mergeObject(foundry.utils.duplicate(itemData), 
-        {
-            type: "feat",
-            system: {
-                actionType: "save",
-                damage: { parts: damageParts },
-                save: { dc: itemData.parent?.system?.attributes?.spelldc },
-                components: { concentration: false },
-                preparation: { mode: "innate", prepared: true },
-                properties: [], // remove properties so we don't copy the concentration property over
-            },
-            effects: [], // no shield guardian animation
-            flags: { "midi-qol": { effectActivation: false, onUseMacroName: "" } }
-        });
-    setProperty(itemUpdate.flags, "autoanimations.killAnim", true);
+// Stage types as of Midi-QOL 13.0.27
+const WORKFLOW_STAGE = {
+    preTargetting: 1,
+    preItemRoll: 2,
+    postNoAction: 3,
+    preStart: 4,
+    postStart: 5,
+    preAoETargetConfirmation: 6,
+    postAoETargetConfirmation: 7,
+    preValidateRoll: 8,
+    postValidateRoll: 9,
+    prePreambleComplete: 10,
+    postPreambleComplete: 11,
+    preWaitForAttackRoll: 12,
+    preAttackRoll: 13,
+    preAttackRollConfig: 14,
+    postWaitForAttackRoll: 15,
+    preAttackRollcomplete: 16,
+    preCheckHits: 17,
+    postAttackRoll: 18,
+    postAttackRollComplete: 19,
+    preWaitForDamageRoll: 20,
+    preDamageRoll: 21,
+    preDamageRollConfig: 22,
+    postWaitForDamageRoll: 23,
+    preConfirmRoll: 24,
+    postConfirmRoll: 25,
+    preDamageRollStarted: 26,
+    DamageBonus: 27,
+    postDamageRollStarted: 28,
+    postDamageRoll: 29,
+    postDamageRollComplete: 30,
+    preWaitForSaves: 31,
+    preSave: 32,
+    postSavesComplete: 33,
+    preAllRollsComplete: 34,
+    preDamageApplication: 35,
+    postAllRollsComplete: 36,
+    preApplyDynamicEffects: 37,
+    preActiveEffects: 38,
+    postApplyDynamicEffects: 39,
+    preRollFinished: 40,
+    postActiveEffects: 41,
+    postRollFinished: 42,
+    preCleanup: 43,
+    postCleanup: 44,
+    preCompleted: 45
+};
 
-    const itemRoll = new CONFIG.Item.documentClass(itemUpdate, { parent: itemData.parent });
-    const options = { showFullCard: false, createWorkflow: true, versatile: false, configureDialog: false, targetUuids: [token.document.uuid], workflowOptions: { 'autoRollDamage': 'always' } };
-    await MidiQOL.completeItemUse(itemRoll, {}, options);
+async function isSlain(workflow) {
+    if (WORKFLOW_STAGE[workflow.macroPass] < WORKFLOW_STAGE.preDamageApplication) {
+        throw new Error(`isSlain is being checked in the ${workflow.macroPass} stage which is too soon`);
+    }
+    const damageItem = workflow.damageItem;
+    return (damageItem.newHP == 0) && (damageItem.oldHP !== 0);
 }
 
-export const workflowApi = { itemDamageToken };
+export const workflowApi = { WORKFLOW_STAGE, isSlain };
